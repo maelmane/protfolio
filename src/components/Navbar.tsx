b94049cc5f37ff8e3d788f996080170dbfa8161f
maelmane <maelmane@outlook.com>
2026-02-05 15:03:57 -0500

    add resume page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
-import React from "react";
 import dayjs from "dayjs";
 
-import {navLinks, navIcons} from "@/constants"
+import { navLinks, navIcons } from "@/constants";
+import useWindowStore from "@/store/window";
 
 const Navbar = () => {
+  const { openWindow } = useWindowStore();
   return (
     <nav>
       <div>
         <img src="/images/logo.svg" alt="logo" />
         <p className="font-bold">Mael's Portfolio</p>
         <ul>
-          {navLinks.map(({ id, name }) => (
-            <li key={id}>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
               <p>{name}</p>
             </li>
           ))}
